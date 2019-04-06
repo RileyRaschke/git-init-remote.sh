67637d5215350bd1766ffab84b82442ddba3784f
Riley Raschke <riley@rrappsdev.com>
2019-04-05 23:06:39 -0500

    Added local and remote repository base configs

--- git-init-remote.sh
+++ git-init-remote.sh
@@ -3,7 +3,8 @@
 test -e $(which git) || { echo "No git executable found on your path, please install git!" >&2 ; exit 1; }
 
 DEFAULT_HOST='scm'
-DEFAULT_BASE="/var/git/$USER"
+DEFAULT_LOCAL_BASE="~/git"
+DEFAULT_REMOTE_BASE="/var/git/$USER"
 DEFAULT_REMOTE_EXT='.git'
 DEFAULT_REMOTE_ENV='/some/env/file' # probably needs full path... maybe has home context.. maybe not.
 
@@ -15,7 +16,8 @@ test -f "$rc" && . "$rc"
 
 # Or default...
 scmHost="${scmHost:-$DEFAULT_HOST}"
-repoBase="${repoBase:-$DEFAULT_BASE}"
+localRepoBase="${localRepoBase:-$DEFAULT_LOCAL_BASE}"
+remoteRepoBase="${remoteRepoBase:-$DEFAULT_REMOTE_BASE}"
 remoteEnvFile="${remoteEnvFile:-$DEFAULT_REMOTE_ENV}"
 remoteExt="${remoteExt:-$DEFAULT_REMOTE_EXT}"
 
@@ -32,7 +34,8 @@ then
 
   echo "# resolved config:
 scmHost=${scmHost}
-repoBase=${repoBase}
+localRepoBase=${localRepoBase}
+remoteRepoBase=${remoteRepoBase}
 remoteEnvFile=${remoteEnvFile}
 remoteExt=${remoteExt}
 continueOnError=1 #leave null to die after remote errors occur
@@ -40,38 +43,40 @@ GIT_INIT_REMOTE_CONFIGED=1
 ##" >&2
 fi
 
-repoPath=$(echo "${repoBase}/$1" | sed "s/\/$(basename "$1")\$//")
+localRepoPath=$(echo "${localRepoBase}/$1" | sed "s/\/$(basename "$1")\$//")
+remoteRepoPath=$(echo "${remoteRepoBase}/$1" | sed "s/\/$(basename "$1")\$//")
 repoName=$(basename "$1")
-repo="${repoPath}/${repoName}"
-remoteRepo="${repo}${remoteExt}"
+localRepo="${localRepoPath}/${repoName}"
+remoteRepo="${remoteRepoPath}/${repoName}${remoteExt}"
 
 echo "Resolved config:"
 echo -e "\tscmHost=$scmHost"
-echo -e "\trepoBase=$repoBase"
+echo -e "\tlocalRepoBase=$localRepoBase"
+echo -e "\tremoteRepoBase=$remoteRepoBase"
 echo -e "\trepoName=$repoName"
-echo -e "\trepoPath=$repoPath"
+echo -e "\tlocalRepoPath=$localRepoPath"
+echo -e "\tremoteRepoPath=$remoteRepoPath"
 echo -e "\tremoteRepo=$remoteRepo"
-echo -e "\trepo=$repo\n"
 
 remoteCmd=$(echo -e "/bin/bash -c \
    \"test -r '${remoteEnvFile}' && . '${remoteEnvFile}' ; \
   test -d '${remoteRepo}' && { echo 'WARN: Remote repo: ${remoteRepo} already exists!' >&2 ; exit 1; } \
-  || test -d '${repoPath}' \
-  || { mkdir -p '${repoPath}' && git init --bare '${remoteRepo}' ; } \
+  || test -d '${remoteRepoPath}' \
+  || { mkdir -p '${remoteRepoPath}' && git init --bare '${remoteRepo}' ; } \
   && { test  -d '${remoteRepo}' || git init --bare '${remoteRepo}' ; }\" "\
 )
 #echo -e "$remoteCmd"
 
 if [ ! -z "$repoName" ]
 then
-  echo "Trying to initialize: ssh://${scmHost}${repo}.git"
+  echo "Trying to initialize: ssh://${scmHost}${remoteRepo}"
   ssh $scmHost "${remoteCmd}" || { test -z "${continueOnError}" && { echo "Errors Occured... Exiting." >&2 ; exit 1; } ; }
 
-  test -d "$repo" && { echo "WARN: Local repostiory already exists! $repo" >&2 ; exit 1; } \
-    || echo "Trying clone from: ssh://${scmHost}${repo}.git" \
-    && echo "in to: ${repo}"  \
-    && test -d "${repoPath}" || mkdir -p "${repoPath}" \
-    && { test -d "${repo}" || git clone "ssh://${scmHost}${repo}.git" "${repo}" ; }
+  test -d "$localRepo" && { echo "WARN: Local repostiory already exists! $localRepo" >&2 ; exit 1; } \
+    || echo "Trying clone from: ssh://${scmHost}${remoteRepo}" \
+    && echo "in to: ${localRepo}"  \
+    && test -d "${localRepoPath}" || mkdir -p "${localRepoPath}" \
+    && { test -d "${localRepo}" || git clone "ssh://${scmHost}${remoteRepo}" "${localRepo}" ; }
 
 else
   echo -e "Usage: $0 repositoryName\n"
